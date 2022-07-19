3e50e83555f11715f830b39c55e7e90b272b1e83
jacksonohallo <jacksonohallo@gmail.com>
2022-07-19 15:33:30 +0300

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
+
+  
+function handleAddItem(newItem) {
+  setItems([...items, newItem]);
+}
+  function handleAddItem(newItem) {
+    console.log("In ShoppingList:", newItem);
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -17,10 +32,12 @@ function ShoppingList() {
     return item.category === selectedCategory;
   });
 
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter
+      {/* add the onAddItem prop! */}
+      <ItemForm onAddItem={handleAddItem} />
+       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
